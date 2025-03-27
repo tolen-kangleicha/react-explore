54329f75a11df4b87c3b45d8ee0f9f3446affc6c
tolen-kangleicha <tolenkangleicha@gmail.com>
2025-03-27 22:40:53 +0530

    Initial commit

---
+++ my-first-react-app/src/App.jsx
@@ -0,0 +1,13 @@
+import Greeting from "./components/Greeting"; // Import the new component
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Greeting />
+      <p>Welcome to my first React project!</p>
+    </div>
+  );
+}
+
+export default App;
